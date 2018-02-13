77f31816decb1349df8d4bf248e79f4adc936347
IsaevEA <IsaevEA@materiamedica.ru>
2018-02-13 11:46:32 +0300

    edit print data 2

--- application/views/BaseLK/print/print_data2.php
+++ application/views/BaseLK/print/print_data2.php
@@ -166,7 +166,7 @@
 </table>
 
 <script type="text/javascript">
-	/*var is_chrome = function () {
+	var is_chrome = function () {
 		return Boolean(window.chrome);
 	};
 
@@ -183,5 +183,5 @@
 			window.print();
 			self.close();
 		});
-	}*/
+	}
 </script>
